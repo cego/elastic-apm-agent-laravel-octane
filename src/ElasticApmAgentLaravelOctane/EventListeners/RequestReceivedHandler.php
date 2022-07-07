1d4f5e835c9a4538cc38a3bac864a41148711724
Niki Ewald Zakariassen <niza@cego.dk>
2022-07-07 11:11:41 +0200

    Fixed get route uri

--- src/ElasticApmAgentLaravelOctane/EventListeners/RequestReceivedHandler.php
+++ src/ElasticApmAgentLaravelOctane/EventListeners/RequestReceivedHandler.php
@@ -2,6 +2,8 @@
 
 namespace Cego\ElasticApmAgentLaravelOctane\EventListeners;
 
+use Throwable;
+use Illuminate\Routing\Router;
 use Laravel\Octane\Events\RequestReceived;
 use Cego\ElasticApmAgentLaravelOctane\OctaneApmManager;
 
@@ -18,7 +20,27 @@ class RequestReceivedHandler
     {
         $manager = $event->sandbox->make(OctaneApmManager::class);
 
-        $manager->beginTransaction($event->request->method() . ' ' . $event->request->route()->uri(), 'request');
+        $manager->beginTransaction($event->request->method() . ' ' . $this->getRouteUri($event), 'request');
         $manager->beginAndStoreSpan('RequestResponse', 'request');
+    }
+
+    /**
+     * Returns the request route uri
+     *
+     * @param RequestReceived $event
+     *
+     * @return string
+     */
+    private function getRouteUri(RequestReceived $event): string
+    {
+        /** @var Router $router */
+        $router = $event->sandbox->make('router');
+
+        try {
+            return $router->getRoutes()->match($event->request)->uri();
+        } catch (Throwable $throwable) {
+            // If the route does not exist, then simply return the path
+            return $event->request->path();
+        }
     }
 }
