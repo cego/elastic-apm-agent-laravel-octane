77e0cf8fe580c85a4cd048c6440a4e86a3f1abd4
Niki Zakariassen <niza@cego.dk>
2023-05-02 11:12:42 +0200

    Added better seeding of random functions for APM

--- src/ElasticApmAgentLaravelOctane/EventListeners/RequestWorkerStartHandler.php
+++ src/ElasticApmAgentLaravelOctane/EventListeners/RequestWorkerStartHandler.php
@@ -19,7 +19,13 @@ class RequestWorkerStartHandler
         if( ! class_exists(ElasticApm::class)){
             return;
         }
-        
+
+        // APM uses mt_rand as a random source for transaction ids, which is seeded by system time.
+        // When multiple workers start at the same time, there is a high chance of transaction id collisions.
+        // Which breaks the APM view.
+        // Therefore, we randomly seed the mt_rand method with a cryptographically secure source to avoid this.
+        mt_srand(random_int(PHP_INT_MIN, PHP_INT_MAX));
+
         $transaction = ElasticApm::getCurrentTransaction();
 
         if ( ! $transaction->hasEnded()) {
